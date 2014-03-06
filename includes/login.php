835211fb6d2cf7c2cee67bf8a2f82dfa0f1c15dc
Onekanda <onekanda@ubuntu.(none)>
2014-03-06 13:19:11 +0100

    new pages

---
+++ includes/login.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>Blog in PHP 2014</title>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+<body>
+    <!--#container-->
+    <div id="container">
+        
+        <!--#head-->
+        <div id="head">
+            <!--#logo-->
+            <div id="logo"><a href="../index.php">Onekblog</a></div>
+            <!--#end logo-->
+            <!--#menu-->
+            <div id="mainmenu">
+                <ul>
+                    <li><a href="../index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="login.php">Login</a></li> 
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            <!--#end menu-->    
+            </div>        
+        </div>
+        <!--end head-->
+        
+        <!--#content-->
+        <div id="content">
+            <!--#content-top-->
+            <div id="content-top">
+                <h1>About us</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet felis orci. Nulla justo tortor, congue in augue vel,
+                placerat pellentesque est. Quisque et consectetur nisl. Nulla facilisi. Proin id rutrum nulla
+                </p>
+                 <p>Aliquam pretium quam eleifend augue iaculis, id imperdiet velit venenatis. Curabitur porttitor magna vel dui vulputate, ac mattis erat vehicula.
+                    Aliquam pretium quam eleifend augue iaculis, id imperdiet velit venenatis. Curabitur porttitor magna vel dui vulputate, ac mattis erat vehicula.
+                 </p>
+            </div>
+        <!--end #content-top-->
+        
+        <!--#content-bottom-->
+        <div id="content-botton">
+            <h3 class="h3-text">
+                <p>about me</p>
+            </h3>
+            <h3 class="h3-text">
+                <p>contact</p>
+            </h3>
+            <h3 class="h3-text">
+                <p>Onekblog</p>
+            </h3>
+          
+        <!--end #content-bottom-->   
+        </div>
+        
+        <!--#footer-->
+        <div id="footer">
+            <p>&copy;2014| Design by Mario Onekanda | Email <a href="mailto:mario.onekanda@gmail.com">mario.onekanda@gmail.com</a></p>
+        </div>
+        <!--end #footer -->
+        
+        <!--end #content-->    
+        </div>
+    </div>
+</body>
+</html>
